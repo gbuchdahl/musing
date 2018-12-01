ffc35b1c55291040669b7f031b2ab55d422c66a9
jzwang8 <jzwang8@gmail.com>
2018-12-01 13:19:32 -0500

    started registration page

--- src/App.js
+++ src/App.js
@@ -5,11 +5,30 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1>Musing</h1>
-          <h2>Maansi!</h2>
+      <div className="app">
+        <header>
+          <div className="wrapper">
+            <h1>Musing</h1>
+          </div>
         </header>
+        <div className='container'>
+          <section className='add-user'>
+            <form>
+              <input type="text" name="firstname" placeholder="First Name"/>
+              <input type="text" name="lastname" placeholder="Last Name"/>
+              <input type="text" name="username" placeholder="Username"/>
+              <input type="text" name="password" placeholder="Password"/>
+              {/*radio button>*/}
+              <input type="radio" id="userType1" name="usertype" value="student"/>
+              <label for="userType1">Student</label>
+
+              <input type="radio" id="userType2"name="usertype" value="teacher"/>
+              <label for="userType2">Teacher</label>
+
+              <button>Register</button>
+            </form>
+          </section>
+        </div>
       </div>
     );
   }
