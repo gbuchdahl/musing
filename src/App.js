5fda4d504cd030fc857521024478139e5d26a2ad
maansi-dasari <dasari.maansi@gmail.com>
2018-11-30 22:01:05 -0500

    added maansi header

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <h1>Musing</h1>
+          <h2>Maansi!</h2>
         </header>
       </div>
     );
