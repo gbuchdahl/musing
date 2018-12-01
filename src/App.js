743f87dceb73a3b2d8fe889f28e2dc4a1b3de12d
jzwang8 <jzwang8@gmail.com>
2018-12-01 13:46:16 -0500

    connected form to state

--- src/App.js
+++ src/App.js
@@ -3,6 +3,17 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      firstname: '',
+      lastname: '',
+      password: '',
+      usertype: '',
+      username: ''
+    }
+    this.handleChange = this.handleChange.bind(this);
+  }
   render() {
     return (
       <div className="app">
@@ -14,15 +25,15 @@ class App extends Component {
         <div className='container'>
           <section className='add-user'>
             <form>
-              <input type="text" name="firstname" placeholder="First Name"/>
-              <input type="text" name="lastname" placeholder="Last Name"/>
-              <input type="text" name="username" placeholder="Username"/>
-              <input type="text" name="password" placeholder="Password"/>
+              <input type="text" name="firstname" placeholder="First Name" onChange={this.handleChange} value={this.state.firstname}/>
+              <input type="text" name="lastname" placeholder="Last Name" onChange={this.handleChange} value={this.state.lastname}/>
+              <input type="text" name="username" placeholder="Username" onChange={this.handleChange} value={this.state.username}/>
+              <input type="text" name="password" placeholder="Password" onChange={this.handleChange} value={this.state.password}/>
               {/*radio button>*/}
-              <input type="radio" id="userType1" name="usertype" value="student"/>
+              <input type="radio" id="userType1" name="usertype" onChange={this.handleChange} value="student"/>
               <label for="userType1">Student</label>
 
-              <input type="radio" id="userType2"name="usertype" value="teacher"/>
+              <input type="radio" id="userType2"name="usertype" onChange={this.handleChange} value="teacher"/>
               <label for="userType2">Teacher</label>
 
               <button>Register</button>
@@ -31,7 +42,14 @@ class App extends Component {
         </div>
       </div>
     );
-  }
+    }
+
+    handleChange(e) {
+      this.setState({
+        [e.target.name]: e.target.value
+      });
+    }
+
 }
 
 export default App;
